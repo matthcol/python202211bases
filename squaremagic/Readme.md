78225af23982b8fa9c832921423bf7f2598c2f8c
matthcol <matthias.colin@gmail.com>
2022-11-29 14:00:44 +0100

    prototype magic square

---
+++ squaremagic/Readme.md
@@ -0,0 +1,14 @@
+# Magic Square
+https://fr.wikipedia.org/wiki/Carr%C3%A9_magique_(math%C3%A9matiques)
+
+# Rules
+1 - magic sum (if all rules are applied)
+magic_sum = n (n^2 + 1) / 2
+
+2 - on all rows, sum(row) = magic_sum 
+
+3 - on all columns, sum(column) = magic_sum
+
+4 - for diagonal, diagonal symetric square, sum(diagonal) = magic_sum
+
+5 - use only once each number in [1 - n^2]
---
+++ squaremagic/magicsquare.py
@@ -0,0 +1,26 @@
+
+def magic_sum(n):
+    """ Compute magic sum of a square of size n.
+        Use formula n(n^2+1)/2
+    """
+    # TODO
+
+def is_magic_square_row(square):
+    """ check if all rows are magic
+    """
+
+def is_magic_square_column(square):
+    """ check if all columns are magic
+    """
+
+def is_magic_square_diagonal(square):
+    """ check if both 'diagonals' are magic
+    """
+
+def is_magic_square_all_present(square):
+    """ check if all numbers between 1 and n^2 are used 
+    (only once)"""
+
+def is_magic_square(square):
+    """ check if all rules are verified to be a magic square
+    """
