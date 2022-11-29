927615279015343aaaf46f3c676e3c53a3cdb394
matthcol <matthias.colin@gmail.com>
2022-11-29 17:11:42 +0100

    check magic ok  + tests

--- squaremagic/magicsquare.py
+++ squaremagic/magicsquare.py
@@ -3,24 +3,78 @@ def magic_sum(n):
     """ Compute magic sum of a square of size n.
         Use formula n(n^2+1)/2
     """
-    # TODO
+    return n * (n**2 + 1) // 2
 
 def is_magic_square_row(square):
     """ check if all rows are magic
     """
+    n = len(square)
+    ms = magic_sum(n)
+    for row in square:
+        row_sum = sum(row)
+        if row_sum != ms:
+            return False
+    return True
+
 
 def is_magic_square_column(square):
     """ check if all columns are magic
     """
+    n = len(square)
+    ms = magic_sum(n)
+    for j in range(n):
+        # compute sum over column j
+        # * sol1: recode sum
+        # column_sum = 0
+        # for i in range(n):
+        #     column_sum += square[i][j]
+        # * sol2: use sum
+        column_sum = sum(square[i][j] for i in range(n))
+        # check sum is magic
+        if column_sum != ms:
+            return False
+    return True
+
+
 
 def is_magic_square_diagonal(square):
     """ check if both 'diagonals' are magic
     """
+    n = len(square)
+    ms = magic_sum(n)
+    # diagonal 1 (main)
+    # NB: you can use sum + for expr here
+    diag_sum = 0
+    for i in range(n):
+        diag_sum += square[i][i]
+    if diag_sum != ms:
+        return False
+    # diagonal 2 (symetric)
+    # NB: you can use sum + for expr here
+    diag_sum = 0
+    for i in range(n):
+        diag_sum += square[i][n-1-i]
+    if diag_sum != ms:
+        return False
+    return True
 
 def is_magic_square_all_present(square):
     """ check if all numbers between 1 and n^2 are used 
     (only once)"""
+    n = len(square)
+    numbers = list(range(1,n**2+1))
+    for i in range(n):
+        for j in range(n):
+            if square[i][j] in numbers:
+                numbers.remove(square[i][j])
+            else:
+                return False
+    return len(numbers) == 0
 
 def is_magic_square(square):
     """ check if all rules are verified to be a magic square
     """
+    return (is_magic_square_row(square)
+        and is_magic_square_column(square)
+        and is_magic_square_diagonal(square)
+        and is_magic_square_all_present(square))
--- squaremagic/test_magicsquare.py
+++ squaremagic/test_magicsquare.py
@@ -1,4 +1,5 @@
 import magicsquare as ms
+import pytest
 
 def test_magic_sum_3():
     res = ms.magic_sum(3)
@@ -20,6 +21,36 @@ square3_ko_row = [
         [4, 7, 8],
     ]
 
+square3_ko_column = [
+        [2, 7, 6],
+        [1, 5, 9],
+        [4, 3, 8],
+    ]
+
+square3_ko_diag = [
+        [9, 5, 1],
+        [2, 7, 6],
+        [4, 3, 8],
+    ]
+
+square3_ko_not_all_numbers_repeat_one = [
+        [5,5,5],
+        [5,5,5],
+        [5,5,5],
+]
+
+square3_ko_not_all_numbers_too_high = [
+        [2, 7, 6],
+        [9, 5, 1],
+        [4, 3, 10],
+]
+
+square3_ko_not_all_numbers_too_low = [
+        [2, 7, 6],
+        [9, 5, 1],
+        [4, 0, 8],
+]
+
 def test_is_magic_square_row_ok():
     res = ms.is_magic_square_row(square3_ok)
     assert res is True
@@ -27,3 +58,56 @@ def test_is_magic_square_row_ok():
 def test_is_magic_square_row_ko():
     res = ms.is_magic_square_row(square3_ko_row)
     assert res is False
+
+def test_is_magic_square_column_ok():
+    res = ms.is_magic_square_column(square3_ok)
+    assert res is True
+
+def test_is_magic_square_column_ko():
+    res = ms.is_magic_square_column(square3_ko_column)
+    assert res is False
+
+def test_is_magic_square_diagonal_ok():
+    res = ms.is_magic_square_diagonal(square3_ok)
+    assert res is True
+
+def test_is_magic_square_diagonal_ko():
+    res = ms.is_magic_square_diagonal(square3_ko_diag)
+    assert res is False
+
+def test_is_magic_square_all_present_ok():
+    res = ms.is_magic_square_all_present(square3_ok)
+    assert res is True
+
+
+@pytest.mark.parametrize("square_ko", [
+        square3_ko_not_all_numbers_repeat_one,
+        square3_ko_not_all_numbers_too_high,
+        square3_ko_not_all_numbers_too_low
+])
+def test_is_magic_square_all_present_ko(square_ko):
+    res = ms.is_magic_square_all_present(square_ko)
+    assert res is False
+
+# Test Integration
+@pytest.mark.parametrize("square_ok", [
+    square3_ok,
+    # square4_albrecht_durer,
+    # square8_general_cazalas
+])
+def test_is_magic_ok(square_ok):
+    res = ms.is_magic_square(square_ok)
+    assert res is True
+
+@pytest.mark.parametrize("square_ko", [
+    square3_ko_row,
+    square3_ko_column,
+    square3_ko_diag,
+    square3_ko_not_all_numbers_repeat_one,
+    square3_ko_not_all_numbers_too_high,
+    square3_ko_not_all_numbers_too_low
+])
+def test_is_magic_ok(square_ko):
+    res = ms.is_magic_square(square_ko)
+    assert res is False
+
