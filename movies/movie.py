052c4d057721e47d9071927fbf75613b78abd6c3
matthcol <matthias.colin@gmail.com>
2022-12-01 12:36:07 +0100

    sort and <

--- movies/movie.py
+++ movies/movie.py
@@ -1,3 +1,5 @@
+import operator as op
+
 class Movie:
     
     def __init__(self, title, year, duration=None):
@@ -21,10 +23,35 @@ class Movie:
             return NotImplemented
         return (self.title, self.year) == (other.title, other.year)
 
+    def __lt__(self, other):
+        """ override < operator
+            Example:
+                movie1 < movie2
+        """
+        if not isinstance(other, Movie):
+            return NotImplemented
+        return (self.year, self.title) < (other.year, other.title)
+        
+
 if __name__ == '__main__':
+    m0 = Movie("Z", 1969)
     m1 = Movie('E.T.', 1982)
     m2 = Movie('Tenet', 2020, duration=150)
-    movies = [m1, m2]
+    m3 = Movie("The Lion King", 1994)
+    m4 = Movie("Pulp Fiction", 1994)
+    m5 = Movie("The Lion King", 2019)
+    m6 = Movie("Léon", 1994)
+    movies = [m0, m1, m2, m3, m4, m5, m6]
     for m in movies:
         print("\t-", m, "; length:", len(m))
     print("m1 == m2 ?", m1 == m2)
+    # sort with internal <
+    movies.sort()
+    print(movies)
+    # sort by title only with key: m => m.title
+    movies.sort(key=lambda m: m.title)
+    print(movies)
+    movies.sort(key=op.attrgetter('title'))
+    print(movies)
+
+
